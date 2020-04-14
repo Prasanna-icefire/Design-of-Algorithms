33683e212be4509502c2920e4794b56d6261a9e9
Prasanna-icefire <prasannamg3@gmail.com>
2020-04-14 21:50:15 +0530

    understood the knapsack problem. Yet to code

---
+++ knapsack.c
@@ -0,0 +1,5 @@
+/*
+Design and develop a program in C to find solution for Knapsack problem using dynamic programming. 
+Determine the time required to find the solution. 
+Repeat the experiment for different values of n and plot a graph of the time taken versus n (n=no of items).
+*/
