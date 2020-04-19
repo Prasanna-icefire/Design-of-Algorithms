3bfcc219c040cd61f241cad9614ec89d86fbb58b
Prasanna-icefire <prasannamg3@gmail.com>
2020-04-19 23:19:33 +0530

    Successfully sorted based on weights

--- knapsack.c
+++ knapsack.c
@@ -5,34 +5,56 @@ Repeat the experiment for different values of n and plot a graph of the time tak
 */
 #include<stdio.h>
 void knapsack(int n,int v[],int w[],int c)
-{   int i=0,r[n],a[n],it[n],j;//a carries the ascending ordered value
+{   int i=0,a[n],it[n],j;//a carries the ascending ordered value
     //Get the value/weight ratio
+    printf("Value of n %d\n",n);
+    float r[n];
     for(i=0;i<n;i++)
     {   
-        r[i]=v[i]/w[i];
-        it[i]=i;
+        r[i]=(float)v[i]/w[i];
+        it[i]=i+1;
     }
-    for(i=0;i<n;i++)
+    //printf("Rates calculated respecticely \n");
+    //for(i=0;i<n;i++)
+      //  printf("%f ",r[i]);
+    for(i=0;i<n-1;i++)
     {
-        for(j=0;j<i;j++)
-        {
-            if(r[j]>r[j++])//Arrenged all the items in ascending order of the rate and in the it obtained the values of items
-            {
+        for(j=0;j<n-i-1;j++)
+        {   
+            if(r[j]>r[j+1])//Arrenged all the items in ascending order of the rate and in the it obtained the values of items + corresponding weights taken down
+            {   printf("Executing Swapp");
                 int temp=it[j];
-                it[j]=it[j++];
-                it[j++]=temp;
+                it[j]=it[j+1];
+                it[j+1]=temp;
                 float t=r[j];
-                r[j]=r[j++];
-                r[j++]=t;
+                r[j]=r[j+1];
+                r[j+1]=t;
+                int we=w[j];
+                w[j]=w[j+1];
+                w[j+1]=we;
             }
         }
     }
-
-
-    while(c>0)
+    printf("Following is the data after arrenging in ascending order:\n");
+    printf("ITEM_NO         RATE     WEIGHT\n");
+    for(i=0;i<n;i++)
     {
+        printf("     %d       %f       %d\n",it[i],r[i],w[i]);
+    }
+    int item[n],correswt[n],pft[n],k=0;
+    for(i=n-1;i>=0;i--)
+    {
+        while(c>=w[i])
+        {
+            item[k]=it[i];
+            correswt[k]=w[i];
+            c-=correswt[k];
+            pft[k]=r[k]*correswt[k];
+            k++;
+        } 
         
-    }    
+    }   
+
 }
 
 void main()
@@ -54,6 +76,5 @@ void main()
     printf("Enter the knapsack bag capacity ");
     scanf("%d",&c);
     printf("Calculating best knapsack items with their respective weights to be carried..\n");
-    printf("Your expected result for maximising profits \n");
     knapsack(n,v,w,c);
 }
