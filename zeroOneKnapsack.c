692ac39ac3fc399e6ba6ed84aa1d1ffbb0a28f29
Prasanna-icefire <prasannamg3@gmail.com>
2020-05-21 20:17:22 +0530

    Accepted net weight

--- zeroOneKnapsack.c
+++ zeroOneKnapsack.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int n;
+int table[10][10];
 int max(int a,int b)
 {
     return((a>b)?a:b);
 }
 void main()
 {
-  int i;
+  int i,nw;
   int p[10],w[10];
   printf("Enter the number of items ");
   scanf("%d",&n);
@@ -18,5 +19,7 @@ void main()
       printf("Weight : ");
       scanf("%d",&w[i]);      
   }
+  printf("Enter the mac weight capacity of the bag : ");
+  scanf("%d",&nw);
 
 }
